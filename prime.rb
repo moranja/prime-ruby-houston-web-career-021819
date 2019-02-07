085e3a8092563c20f58a65235de654edd4b7966c
Adam Moran <jeffadammoran@gmail.com>
2019-02-07 19:58:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime? (potential_prime)
   if potential_prime < 2 
     return FALSE
   else
-  countdown = [1..potential_prime]
+  countdown = Array(1..potential_prime)
     countdown.each do |i|
       if potential_prime % i == 0 
         return FALSE
