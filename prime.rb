a0ddc5d486702b384f03f25e593fe40d17093943
Adam Moran <jeffadammoran@gmail.com>
2019-02-07 20:01:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,10 @@
 def prime? (potential_prime)
-  if potential_prime < 2 
-    return FALSE
-  elsif potential_prime == 2
-    return TRUE
+  if potential_prime < 2     {return FALSE}
+  elsif potential_prime == 2 {return TRUE}
   else
   countdown = Array(2..potential_prime-1)
     countdown.each do |i|
-      if potential_prime % i == 0 
+      if potential_prime % i == 0
         return FALSE
         break
       end
